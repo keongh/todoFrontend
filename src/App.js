c71362e284dadefc61005ba798a98bbceff87850
keon <kghadamyari@gmail.com>
2020-08-19 13:08:34 -0400

    Basic UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <h2>My Todo App</h2>
+          <form>
+            <input placeholder="New item"></input>
+            <button type="submit">Submit</button>
+          </form>
+          <h4>Todo</h4>
+        </div>
+      );
+  }
 }
-
-export default App;
